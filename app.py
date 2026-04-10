0e0f298c935bcec57c082b1f3fb9b9e6ab866030
codeverse-25 <codeverse2k25@gmail.com>
2026-04-10 08:11:02 +0530

    fix: absolute lowercase standardization for cloud deployment navigation

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import streamlit as st
 
 pages = [
     st.Page("pages/1_Complaint_Register.py", title="Complaint Register"),
-    st.Page("pages/2_Government_panel.py", title="Government Panel"),
-    st.Page("pages/3_Complaint_history.py", title="Complaint History"),
-    st.Page("pages/4_Dashboard.py", title="Dashboard"),
+    st.Page("pages/2_government_panel.py", title="Government Panel"),
+    st.Page("pages/3_complaint_history.py", title="Complaint History"),
+    st.Page("pages/4_dashboard.py", title="Dashboard"),
     st.Page("pages/5_About_page.py", title="About Page")
 ]
 
 pg = st.navigation(pages)
 pg.run()
-
-
